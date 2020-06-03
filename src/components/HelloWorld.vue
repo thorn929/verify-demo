c34cfb9eda773575273b7ef22605c515c4ebde47
zhaochuyang <zhaochuyang@yiche.com>
2020-06-03 17:20:40 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,44 @@
 <template>
-  <div
-    class="hello"
-  >
-    <h1>
-      {{
-        msg
-      }}
-    </h1>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
     <p>
-      For
-      a
-      guide
-      and
-      recipesd
-      on
-      how
-      to
-      configure
-      /
-      customize
-      this
-      project,
-      <img
-        alt
-      />
-      <a
-        href="https://cli.vuvuejsvuejsvuejsejs.org"
-        rel="noopener"
-        target="_blank"
-      >
-        vuecli
-        documentation
-      </a>
+      For a guide and recipes on how to configure / customize this project,
+      <img alt />
+      <a href="https://cli.vuejs.org" rel="noopener" target="_blank">vue-cli documentation</a>
     </p>
-    <h3>
-      Installed
-      CLI
-      Plugins
-    </h3>
+    <h3>Installed CLI Plugins</h3>
     <ul>
       <!-- 换行 -->
       <li>
-        <a
-          href="https:/ugin-router"
-          rel="noopener"
-          target="_blank"
-          >router</a
-        >
+        <a href="https:/ugin-router" rel="noopener" target="_blank">router</a>
       </li>
       <!-- br -->
       <br />
     </ul>
-    <h3>
-      Essential
-      Links
-    </h3>
+    <h3>Essential Links</h3>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          rel="noopener"
-          target="_blank"
-        >
-          Core
-          Docs
-        </a>
+        <a href="https://vuejs.org" rel="noopener" target="_blank">Core Docs</a>
       </li>
       <li>
-        <a
-          href="https://twitter.com/vuejs"
-          rel="noopener"
-          target="_blank"
-          >Twitter</a
-        >
+        <a href="https://twitter.com/vuejs" rel="noopener" target="_blank">Twitter</a>
       </li>
       <li>
-        <a
-          href="https://news.vuejs.org"
-          rel="noopener"
-          target="_blank"
-          >News</a
-        >
+        <a href="https://news.vuejs.org" rel="noopener" target="_blank">News</a>
       </li>
     </ul>
-    <h3>
-      Ecosystem
-    </h3>
+    <h3>Ecosystem</h3>
     <ul>
       <li>
-        <a
-          href="https://router.vuejs.org"
-          rel="noopener"
-          target="_blank"
-          >vue-router</a
-        >
+        <a href="https://router.vuejs.org" rel="noopener" target="_blank">vue-router</a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          rel="noopener"
-          target="_blank"
+        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" rel="noopener" target="_blank"
           >vue-devtools</a
         >
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          >awesome-vue</a
-        >
+        <a href="https://github.com/vuejs/awesome-vue">awesome-vue</a>
       </li>
     </ul>
   </div>
@@ -115,39 +46,20 @@
 
 <script>
 export default {
-  name:
-    'HelloWorld',
+  name: 'HelloWorld',
   props: {
     msg: String
   },
   methods: {
     test() {
-      let obj = {
-        a: 2,
-        b: 12
-      }
-      console.log(
-        obj
-      )
-      const x =
-        obj +
-        5
-      console.log(
-        x
-      )
-
-      let obdj = {
-        foo: 42
-      }
-      console.log(
-        obdj
-      )
-      if (
-        obj
-      ) {
-        console.log(
-          3324
-        )
+      let obj = { a: 2, b: 12 }
+      console.log(obj)
+      const x = obj + 5
+      console.log(x)
+      let obdj = { foo: 42 }
+      console.log(obdj)
+      if (obj) {
+        console.log(3324)
       }
     }
   }
@@ -155,13 +67,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style
-  scoped
-  lang="less"
->
+<style scoped lang="less">
 h3 {
-  margin: 40px
-    0 0;
+  margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
@@ -169,8 +77,7 @@ ul {
 }
 li {
   display: inline-block;
-  margin: 0
-    10px;
+  margin: 0 10px;
 }
 a {
   color: #42b983;

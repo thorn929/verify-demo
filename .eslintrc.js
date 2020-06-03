60a796f2353aaf12662b5592775fe6f212ded129
zhaochuyang <zhaochuyang@yiche.com>
2020-06-03 14:48:15 +0800

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,24 @@
+/*
+ * @Author: zcy
+ * @Date: 2020-05-11 16:02:29
+ * @LastEditTime: 2020-06-03 14:47:53
+ * @LastEditors: zcy
+ * @Description: eslint 配置
+ */
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    jquery: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': ['error', 2]
   }
-};
+}
--- README.md
+++ README.md
@@ -22,3 +22,9 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+
+git commit 是最常用的命令之一，它可以触发四个 hook ，分别是 pre-commit, prepare-commit-msg, commit-msg 和 post-commit。 从字面上猜测着四个 hook 分别对应了 “commit 之前”，“准备 commit log message 的时候”，“生成 commit log message 的时候”，“commit 之后”四个阶段。
+而我们要解决我们的问题，其实只需要在 'pre-commit' 这个阶段去写一段脚本，就能解决我们检测代码的问题。
